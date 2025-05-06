c8f0d215087868864a403fe020ab530b5bfd3b59
Lara de Oliveira <147353595+laraffoliv@users.noreply.github.com>
2025-05-06 16:25:00 -0300

    Delete frontend directory

--- frontend/Middleware/checkObjectld.js
+++
@@ -1,9 +0,0 @@
-const checkObjectId = (req, res, next) => {
-    if (!Number.isInteger(Number(req.params.id))) {
-      res.status(404);
-      throw new Error(`Invalid ID: ${req.params.id}`);
-    }
-    next();
-  };
-  
-  export default checkObjectId;
